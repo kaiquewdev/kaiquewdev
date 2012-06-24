41baac9400f65f24acaa3de3161441f26426aef9
Kaique da Silva <kaique.developer@gmail.com>
2012-06-24 18:50:58 -0300

    projects ok

--- public/javascripts/kw.js
+++ public/javascripts/kw.js
@@ -2,6 +2,74 @@ var kw = (function ( window, $ ) {
     var _ = function () {};
 
     _.prototype = {
+        brandAnimationLetters: function ( settings ) {
+            var output = {};
+            settings = settings || {};
+
+            if ( settings ) {
+                var element = $(settings['element']),
+                    timer = 250;
+
+                    element.each(function ( letter ) {
+                        if ( letter > 0 && letter < ( element.length - 1 ) ) {
+                            element.eq( letter ).hide();
+                            
+                            setTimeout(function () {
+                                element.eq( letter ).fadeTo('fast', 1);
+                            }, timer);
+
+                            timer += 50;
+                        }
+                    });
+
+                    output = element;
+            }
+
+            return output;
+        },
+
+        githubProjects: function ( settings ) {
+            // request to https://api.github.com/users/kaiquewdev/repos?type=owner
+            if ( settings ) {
+                var requestURL = 'https://api.github.com/users/kaiquewdev/repos?type=owner&client_id=ad83403be07698072d0a',
+                    element = $( settings['element'] ),
+                    ownRepos = [];
+
+                var data = $.ajax({
+                    url: requestURL,
+                });
+                
+                data.done(function () {
+                    var li = element.find('li'),
+                        timer = 500;
+                        
+                        ownRepos = JSON.parse( data.responseText );
+                    
+                    $('p.message').fadeTo('normal', 0, function () {
+                        $( this ).hide();    
+                    });
+                    
+                    if ( ownRepos ) {
+                        for ( var repo in ownRepos ) {
+                            var currentRepo = ownRepos[repo];
+
+                            if ( !currentRepo['fork'] ) {
+                                element.append('<li><a href=\"' + currentRepo['html_url'] + '\" target="blank">' + currentRepo['name'] + '</a></li>');
+                            }
+                        }    
+
+                        element.append('<li><a href=\"' + settings['profile'] + '\" target="blank">Perfil no Github</a></li>');
+
+                        element.find('li').hide();
+                        element.find('li').each(function ( id ) {
+                            element.find('li').eq(id).delay( timer ).fadeTo('slow', 1);    
+                            timer += 100;
+                        });
+                    }
+                });
+            }
+        },
+
         init: function ( callback ) {
             // Start
             var self = this;
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,20 @@
 requirejs([
     'javascripts/jquery',
+    'javascripts/jquery.lettering',
     'javascripts/kw'
 ], function () {
     $(function () {
         kw.init(function ( self ) {
-            // work arround
+            $('h1').lettering();
+            // Animation letters of span
+            self.brandAnimationLetters({
+                element: 'div.brand a h1 span',
+            });
+            // Get projects
+            self.githubProjects({
+                element: '.weekend-projects',
+                profile: 'http://github.com/kaiquewdev/'
+            });
         });
     }); 
 });
